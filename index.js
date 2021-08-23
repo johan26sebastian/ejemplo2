4854067c9a50aac248fdafcbc2d00773688e65f7
johann1sebastian <82532258+johan26sebastian@users.noreply.github.com>
2021-08-23 17:57:48 -0500

    3

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-//
+//si pones el ++ primero se suma 1 deuna
+let variable = 0;
+console.log(variable);
+console.log(++variable);
+// resultado
+//0
+//1
+
+//si pones el ++ de ultimo se suma 1 deultimo por lo que aparece la variable primero
+let variable = 0;
+console.log(variable);
+console.log(variable++);
+// resultado:
+//0
+//0
+
+//2.0 en esta al poner otra linea(console.log(variable);)me muestra el valor de la suma=1, mas no muestra el valor de la nueva variable que es igual a =0
+let variable = 0;
+console.log(variable);
+console.log(variable++);
+console.log(variable);
+//resultado:
+//0
+//0
+//1
+
+// Siempre se tiene que identificar la variable o si no:
+console.log(variable);
+console.log(variable++);
+//resultado:
+// ¡¡¡¡¡error¡¡¡¡
+
+alert
